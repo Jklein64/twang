399163274207102f4cf0c4ff8c4b780e35af4d39
Jason Klein <jklein.64@verizon.net>
2024-01-26 17:16:59 -0500

    Add the rest of the colors

--- source/Colors.h
+++ source/Colors.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <juce_graphics/juce_graphics.h>
+
 namespace Colors
 {
     // all colors are RGBA hex, alpha is first value
     const juce::Colour background { 0xFFF5D6E4 };
+    const juce::Colour primary { 0xFF704C5E };
+    const juce::Colour secondary { 0xFFE2B6CA };
 } // namespace colors
