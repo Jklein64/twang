0dce3782ab18bbdd2ff17608c3480ea7bf08cc45
Jason Klein <jklein.64@verizon.net>
2024-01-26 21:11:00 -0500

    Draw initial border

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -22,6 +22,9 @@ PluginEditor::PluginEditor (PluginProcessor& p)
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    setOpaque (true);
+    setResizable (true, true);
 }
 
 PluginEditor::~PluginEditor()
@@ -30,10 +33,33 @@ PluginEditor::~PluginEditor()
 
 void PluginEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    // opaque -> must fill background
     g.fillAll (Colors::background);
 
     auto area = getLocalBounds();
+
+    juce::Path border;
+    int w = 104, h = 136;
+    auto borderArea = area.withSizeKeepingCentre (w, h);
+    auto x = borderArea.getX(), y = borderArea.getY();
+    // start with top left corner bottom section and go clockwise
+    // todo: the path below should be the outer limits
+    border.startNewSubPath (x, y + 8);
+    border.lineTo (x + 8, y);
+    border.lineTo (x + w - 8, y);
+    border.lineTo (x + w, y + 8);
+    border.lineTo (x + w, y + h - 8);
+    border.lineTo (x + w - 8, y + h);
+    border.lineTo (x + 8, y + h);
+    border.lineTo (x, y + h - 8);
+    border.closeSubPath();
+    border.scaleToFit (x + 2, y + 2, w, h, true);
+    g.setColour (Colors::secondary);
+    g.strokePath (border, juce::PathStrokeType (4.0f));
+    border.scaleToFit (x, y, w, h, true);
+    g.setColour (Colors::primary);
+    g.strokePath (border, juce::PathStrokeType (4.0f));
+
     g.setColour (juce::Colours::white);
     g.setFont (16.0f);
     auto helloWorld = juce::String ("Hello from ") + PRODUCT_NAME_WITHOUT_VERSION + " v" VERSION + " running in " + CMAKE_BUILD_TYPE;
