8da3c00e1ded6bc192b7cebdca953b244ad7041e
Jason Klein <jklein.64@verizon.net>
2024-01-26 21:43:08 -0500

    Resize and remove inspector

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -6,19 +6,6 @@ PluginEditor::PluginEditor (PluginProcessor& p)
 {
     juce::ignoreUnused (processorRef);
 
-    addAndMakeVisible (inspectButton);
-
-    // this chunk of code instantiates and opens the melatonin inspector
-    inspectButton.onClick = [&] {
-        if (!inspector)
-        {
-            inspector = std::make_unique<melatonin::Inspector> (*this);
-            inspector->onClose = [this]() { inspector.reset(); };
-        }
-
-        inspector->setVisible (true);
-    };
-
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
@@ -39,37 +26,28 @@ void PluginEditor::paint (juce::Graphics& g)
     auto area = getLocalBounds();
 
     juce::Path border;
-    int w = 104, h = 136;
+    int w = 208, h = 272;
     auto borderArea = area.withSizeKeepingCentre (w, h);
     auto x = borderArea.getX(), y = borderArea.getY();
     // start with top left corner bottom section and go clockwise
     // todo: the path below should be the outer limits
-    border.startNewSubPath (x, y + 8);
-    border.lineTo (x + 8, y);
-    border.lineTo (x + w - 8, y);
-    border.lineTo (x + w, y + 8);
-    border.lineTo (x + w, y + h - 8);
-    border.lineTo (x + w - 8, y + h);
-    border.lineTo (x + 8, y + h);
-    border.lineTo (x, y + h - 8);
+    border.startNewSubPath (x, y + 16);
+    border.lineTo (x + 16, y);
+    border.lineTo (x + w - 16, y);
+    border.lineTo (x + w, y + 16);
+    border.lineTo (x + w, y + h - 16);
+    border.lineTo (x + w - 16, y + h);
+    border.lineTo (x + 16, y + h);
+    border.lineTo (x, y + h - 16);
     border.closeSubPath();
-    border.scaleToFit (x + 2, y + 2, w, h, true);
+    border.scaleToFit (x + 4, y + 4, w, h, true);
     g.setColour (Colors::secondary);
-    g.strokePath (border, juce::PathStrokeType (4.0f));
+    g.strokePath (border, juce::PathStrokeType (8.0f));
     border.scaleToFit (x, y, w, h, true);
     g.setColour (Colors::primary);
-    g.strokePath (border, juce::PathStrokeType (4.0f));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (16.0f);
-    auto helloWorld = juce::String ("Hello from ") + PRODUCT_NAME_WITHOUT_VERSION + " v" VERSION + " running in " + CMAKE_BUILD_TYPE;
-    g.drawText (helloWorld, area.removeFromTop (150), juce::Justification::centred, false);
+    g.strokePath (border, juce::PathStrokeType (8.0f));
 }
 
 void PluginEditor::resized()
 {
-    // layout the positions of your child components here
-    auto area = getLocalBounds();
-    area.removeFromBottom (50);
-    inspectButton.setBounds (getLocalBounds().withSizeKeepingCentre (100, 50));
 }
--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -2,7 +2,6 @@
 
 #include "BinaryData.h"
 #include "PluginProcessor.h"
-#include "melatonin_inspector/melatonin_inspector.h"
 
 //==============================================================================
 class PluginEditor : public juce::AudioProcessorEditor
@@ -19,7 +18,5 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     PluginProcessor& processorRef;
-    std::unique_ptr<melatonin::Inspector> inspector;
-    juce::TextButton inspectButton { "Inspect the UI" };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
 };
